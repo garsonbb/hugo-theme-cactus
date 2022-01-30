0849c61bcdf58c208b3ea2ab98fe5918f31be42f
garson <z1195224900@gmail.com>
2022-01-30 19:05:53 +0800

    add local search

--- static/js/search.js
+++ static/js/search.js
@@ -1,154 +1,97 @@
-// A local search script with the help of
-// [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
-// Copyright (C) 2015
-// Joseph Pan <http://github.com/wzpan>
-// Shuhao Mao <http://github.com/maoshuhao>
-// This library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2.1 of the
-// License, or (at your option) any later version.
+var fuse; // holds our search engine
+var list = document.getElementById('search-result-list'); // targets the <ul>
+var maininput = document.getElementById('search-input'); // input box for search
+var noResults = document.getElementById('search-no-result'); // Did we get any search results?
+
+loadSearch();
+
+// ==========================================
+// execute search as each character is typed
 //
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+document.getElementById("search-input").oninput = function (e) {
+  executeSearch(this.value);
+}
+document.getElementById("search-input").onkeydown = function (e) {
+  if (e.key == 'Enter') { return false; }
+}
+
+// ==========================================
+// fetch some json without jquery
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA
-//
-// Modified by:
-// Pieter Robberechts <http://github.com/probberechts>
-
-/*exported searchFunc*/
-var searchFunc = function(path, searchId, contentId) {
-
-  function stripHtml(html) {
-    html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
-    html = html.replace(/<script([\s\S]*?)<\/script>/gi, "");
-    html = html.replace(/<figure([\s\S]*?)<\/figure>/gi, "");
-    html = html.replace(/<\/div>/ig, "\n");
-    html = html.replace(/<\/li>/ig, "\n");
-    html = html.replace(/<li>/ig, "  *  ");
-    html = html.replace(/<\/ul>/ig, "\n");
-    html = html.replace(/<\/p>/ig, "\n");
-    html = html.replace(/<br\s*[\/]?>/gi, "\n");
-    html = html.replace(/<[^>]+>/ig, "");
-    return html;
-  }
-
-  function getAllCombinations(keywords) {
-    var i, j, result = [];
-
-    for (i = 0; i < keywords.length; i++) {
-        for (j = i + 1; j < keywords.length + 1; j++) {
-            result.push(keywords.slice(i, j).join(" "));
-        }
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        let data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
     }
-    return result;
-  }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
 
-  $.ajax({
-    url: path,
-    dataType: "xml",
-    success: function(xmlResponse) {
-      // get the contents from search data
-      var datas = $("entry", xmlResponse).map(function() {
-        return {
-          title: $("title", this).text(),
-          content: $("content", this).text(),
-          url: $("link", this).attr("href")
-        };
-      }).get();
-
-      var $input = document.getElementById(searchId);
-      if (!$input) { return; }
-      var $resultContent = document.getElementById(contentId);
-
-      $input.addEventListener("input", function(){
-        var resultList = [];
-        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
-          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
-        $resultContent.innerHTML = "";
-        if (this.value.trim().length <= 0) {
-          return;
-        }
-        // perform local searching
-        datas.forEach(function(data) {
-          var matches = 0;
-          if (!data.title || data.title.trim() === "") {
-            data.title = "Untitled";
-          }
-          var dataTitle = data.title.trim().toLowerCase();
-          var dataContent = stripHtml(data.content.trim());
-          var dataUrl = data.url;
-          var indexTitle = -1;
-          var indexContent = -1;
-          var firstOccur = -1;
-          // only match artiles with not empty contents
-          if (dataContent !== "") {
-            keywords.forEach(function(keyword) {
-              indexTitle = dataTitle.indexOf(keyword);
-              indexContent = dataContent.indexOf(keyword);
-
-              if( indexTitle >= 0 || indexContent >= 0 ){
-                matches += 1;
-                if (indexContent < 0) {
-                  indexContent = 0;
-                }
-                if (firstOccur < 0) {
-                  firstOccur = indexContent;
-                }
-              }
-            });
-          }
-          // show search results
-          if (matches > 0) {
-            var searchResult = {};
-            searchResult.rank = matches;
-            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
-            if (firstOccur >= 0) {
-              // cut out 100 characters
-              var start = firstOccur - 20;
-              var end = firstOccur + 80;
-
-              if(start < 0){
-                start = 0;
-              }
-
-              if(start == 0){
-                end = 100;
-              }
-
-              if(end > dataContent.length){
-                end = dataContent.length;
-              }
-
-              var matchContent = dataContent.substr(start, end);
-
-              // highlight all keywords
-              var regS = new RegExp(keywords.join("|"), "gi");
-              matchContent = matchContent.replace(regS, function(keyword) {
-                return "<em class=\"search-keyword\">"+keyword+"</em>";
-              });
-
-              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
-            }
-            searchResult.str += "</li>";
-            resultList.push(searchResult);
-          }
-        });
-        resultList.sort(function(a, b) {
-            return b.rank - a.rank;
-        });
-        var result ="<ul class=\"search-result-list\">";
-        for (var i = 0; i < resultList.length; i++) {
-          result += resultList[i].str;
-        }
-        result += "</ul>";
-        $resultContent.innerHTML = result;
-      });
-    }
+// ==========================================
+// load our search index, only executed once
+// on 
+//
+function loadSearch() {
+  fetchJSONFile('/search/index.json', function (data) {
+    let options = { // fuse.js options; check fuse.js website for details
+      shouldSort: true,
+      distance: 10000,
+      minMatchCharLength: 2,
+      keys: [
+        {
+          name: 'title',
+          weight: 2.5,
+        },
+        {
+          name: 'content',
+          weight: 1.5,
+        },
+        {
+          name: 'permalink',
+          weight: 1.0,
+        },
+      ]
+    };
+    fuse = new Fuse(data, options); // build the index from the json file
   });
-};
+}
+
+// ==========================================
+// using the index we loaded on CMD-/, run 
+// a search query (for "term") every time a letter is typed
+// in the search box
+//
+function executeSearch(term) {
+  let trimKeyword = term.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  let keywordList = trimKeyword.split(/\s+/);
+  let searchPattern = keywordList.join(" | "); // multiple words search
+  let highLightTitle = '';
+  let highLightContent = '';
+
+  let results = fuse.search(searchPattern); // the actual query being run using fuse.js
+  let searchitems = ''; // our results bucket
+
+  if (results.length === 0 && maininput.value.length > 1) { // no results based on what was typed into the input box
+    noResults.style.display = 'block';
+    searchitems = '';
+  } else { // build our html 
+    for (let index in results.slice(0, 10)) { // only show first 10 results
+      let regS = new RegExp(keywordList.join('|'), "gi");
+      highLightContent = results[index].item.content.replace(regS, function (keyword) {
+        return '<em class="search-keyword">' + keyword + "</em>";
+      });
+      highLightTitle = results[index].item.title.replace(regS, function (keyword) {
+        return '<em class="search-keyword">' + keyword + "</em>";
+      });
+      searchitems = searchitems + '<li><a href="' + results[index].item.permalink + '" class="search-result-title">' + highLightTitle + '</a><p class="search-result">' + highLightContent + '</p></li>';
+    }
+    noResults.style.display = 'none';
+  }
+
+  document.getElementById("search-result-list").innerHTML = searchitems;
+}
